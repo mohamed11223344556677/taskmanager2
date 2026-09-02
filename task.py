e7c26c4590dc18786e3689cf44cc349138dc3819
Mohamed Tamer Elnaggar <mohamedtamer@Mohameds-MacBook-Air.local>
2026-09-02 13:49:02 +0300

    add list and remove the exit commands

--- task.py
+++ task.py
@@ -27,3 +27,32 @@ while True:
         tasks.append({"task": task})
         save_tasks(tasks)
         print("task added")
+    elif command == "list":
+        if len(tasks) == 0:
+            print("no tasks")
+        else:
+            for i in range(len(tasks)):
+                print(i + 1, "-", tasks[i]["task"])
+
+    elif command == "remove":
+        if len(tasks) == 0:
+            print("no tasks")
+        else:
+            for i in range(len(tasks)):
+                print(i + 1, "-", tasks[i]["task"])
+
+            number = int(input("enter task number to remove: "))
+
+            if number >= 1 and number <= len(tasks):
+                tasks.pop(number - 1)
+                save_tasks(tasks)
+                print("task removed")
+            else:
+                print("invalid task number")
+
+    elif command == "exit":
+        print("program ended")
+        break
+
+    else:
+        print("invalid command")
