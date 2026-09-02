f4f85da861508baddc57bca5d51e5a9c15d838c0
Mohamed Tamer Elnaggar <mohamedtamer@Mohameds-MacBook-Air.local>
2026-09-02 13:21:30 +0300

    file loading

---
+++ task/task.py
@@ -0,0 +1,16 @@
+import json
+
+filename = "tasks.json"
+
+
+def load_tasks():
+    try:
+        with open(filename, "r") as file:
+            return json.load(file)
+    except FileNotFoundError:
+        with open(filename, "w") as file:
+            json.dump([], file)
+        return []
+
+
+tasks = load_tasks()
